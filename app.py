71bb940e7ac807fd5e2ed7c9c662a5b9b250ccd8
Andrew Belko <spydnet@gmail.com>
2023-04-11 08:22:38 +0300

    test2

--- app.py
+++ app.py
@@ -8,22 +8,21 @@ def index():
     return render_template('index.html')
 
 @app.route('/formats', methods=['POST'])
-def formats():
+def get_formats():
     url = request.form['url']
     yt = YouTube(url)
     available_formats = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc()
-    formats_list = []
-    for stream in available_formats:
-        formats_list.append({'itag': stream.itag, 'resolution': stream.resolution, 'mime_type': stream.mime_type})
-    return jsonify({'formats': formats_list})
+    formats = [{'itag': s.itag, 'resolution': s.resolution, 'mime_type': s.mime_type} for s in available_formats]
+    return jsonify(formats)
 
 @app.route('/download', methods=['GET'])
 def download_video():
     url = request.args.get('url')
     itag = request.args.get('format')
-    video = YouTube(url)
-    stream = video.streams.get_by_itag(itag)
-    return stream.download()
+    yt = YouTube(url)
+    stream = yt.streams.get_by_itag(itag)
+    response = stream.stream_to_buffer()
+    return response
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
