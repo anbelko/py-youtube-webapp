84f14d2b0ae46bdca20f1addacec76db0b03e29f
Andrew Belko <spydnet@gmail.com>
2023-04-11 08:31:18 +0300

    make response from flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, make_response
 from pytube import YouTube
 
 app = Flask(__name__)
