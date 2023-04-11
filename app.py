dd76c54dd2a86bc4a7b1e736d302b479e97aff07
Andrew Belko <spydnet@gmail.com>
2023-04-11 08:47:01 +0300

    test321

--- app.py
+++ app.py
@@ -26,17 +26,21 @@ def download_video():
     url = request.form.get("url")
     format_id = request.form.get("format_id")
 
-    # Get the selected format URL
-    format_url = video_formats[int(format_id)]["url"]
+    yt = YouTube(url)
+    available_formats = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc()
+    formats = [{'itag': s.itag, 'resolution': s.resolution, 'mime_type': s.mime_type, 'url': s.url} for s in available_formats]
+
+    # Find the selected format in the list of available formats
+    selected_format = next((f for f in formats if f['itag'] == format_id), None)
 
     # Set the response headers
     response_headers = {
-        "Content-Type": "video/mp4",
-        "Content-Disposition": "attachment; filename=video.mp4",
+        "Content-Type": selected_format['mime_type'],
+        "Content-Disposition": f"attachment; filename={yt.title}.mp4",
     }
 
-    # Send the request to the format URL
-    r = requests.get(format_url, stream=True)
+    # Send the request to the selected format URL
+    r = requests.get(selected_format['url'], stream=True)
 
     # Create a buffer to store the contents of the response stream
     buffer = io.BytesIO()
@@ -54,7 +58,6 @@ def download_video():
         response.headers[key] = value
 
     return response
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
